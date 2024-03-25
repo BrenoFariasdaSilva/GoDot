ac0167a81a0c461423fb322ade94bf8e7936be70
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-03-25 14:52:11 -0300

    FEAT: Adding a Controls Exported Dictionary to the Third Player Ship

--- 03 - Space Ship - Platform Controls/player_ship.gd
+++ 03 - Space Ship - Platform Controls/player_ship.gd
@@ -4,6 +4,9 @@ class_name PlayerShip
 const SPEED: float = 400
 const ROTATION_SPEED: float = 5
 @onready var _size: Vector2 = $Sprite2D.get_rect().size
+@export var _keys: Dictionary = {
+	"left": "ui_left", "right": "ui_right",
+	"up": "ui_up", "down": "ui_down"}
 
 var _input: Vector2 = Vector2.ZERO
 var _direction: Vector2 = Vector2.ZERO
@@ -12,7 +15,6 @@ var _screen_size: Vector2
 func _ready() -> void:
 	_screen_size = get_viewport_rect().size
 
-
 func _process(_delta: float) -> void:
 	#_move_4_directions(_delta)
 	#_move_4_directions_nonstop(_delta)
@@ -20,57 +22,53 @@ func _process(_delta: float) -> void:
 	_move_and_turn(_delta)
 	
 func _move_and_turn(_delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed(_keys["left"]):
 		rotation -= ROTATION_SPEED * _delta
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed(_keys["right"]):
 		rotation += ROTATION_SPEED * _delta
 		
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed(_keys["up"]):
+		# Ao somar e não simplesmente atribuir, ele mantém a inércia (Astroids).
 		# _input += Vector2.from_angle(rotation) * SPEED * 2 * _delta
 		_input += Vector2(cos(rotation), sin(rotation)) * SPEED * 2 * _delta
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed(_keys["down"]):
 		#_input = _input.move_toward(Vector2.ZERO, SPEED * _delta)
 		_input = _input * 0.95
 		
 	velocity = _input.limit_length(SPEED)
 	move_and_slide()
 	_wrap_screen()
-	
-
 
 func _move_in_any_direction(_delta: float) -> void:
-	_input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	_input = Input.get_vector(_keys["left"], _keys["right"], _keys["up"], _keys["down"])
 
 	_move_and_rotate(_delta)
 
-
 func _move_4_directions_nonstop(_delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed(_keys["left"]):
 		_input = Vector2.LEFT
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed(_keys["right"]):
 		_input = Vector2.RIGHT
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed(_keys["up"]):
 		_input = Vector2.UP
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed(_keys["down"]):
 		_input = Vector2.DOWN
 
 	_move_and_rotate(_delta)	
 
-
 func _move_4_directions(_delta: float) -> void:
 	_input = Vector2.ZERO
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed(_keys["left"]):
 		_input.x = -1
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed(_keys["right"]):
 		_input.x = 1
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed(_keys["up"]):
 		_input.y = -1
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed(_keys["down"]):
 		_input.y = 1
 
 	_move_and_rotate(_delta)
-	
 	
 func _move_and_rotate(delta: float) -> void:
 	#if _input:
@@ -82,7 +80,6 @@ func _move_and_rotate(delta: float) -> void:
 	velocity = _input * SPEED
 	move_and_slide()
 	_wrap_screen()	
-
 
 func _clamp_screen() -> void:
 	position.x = clamp(position.x, _size.x/2, _screen_size.x - _size.x/2)
